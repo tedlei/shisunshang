63f8670d8472585ffaa7946165b605a61181776b
zh_New <739493331@qq.com>
2020-03-24 10:21:10 +0800

    201

--- src/components/children/recommendedShop/recommendedShop.vue
+++ src/components/children/recommendedShop/recommendedShop.vue
@@ -68,7 +68,7 @@
       },
         afterRead (file) {
           // 此时可以自行将文件上传至服务器
-          console.log(file);
+        //   console.log(file);
         },
         afterReadTwo (file) {
           // 此时可以自行将文件上传至服务器
