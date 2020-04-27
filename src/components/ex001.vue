810bb4129b42c403e133e1bfa49190f4d586e68f
ted <1304869679@qq.com>
2020-04-27 15:13:01 +0800

    '获取位置'

--- src/components/ex001.vue
+++ src/components/ex001.vue
@@ -1,127 +1,134 @@
 <template>
-    <div v-if="classlist.length>1">
-        <div></div>
-        <van-tabs v-model="active" @click="onClick">
-            <van-tab :title="item.cate_name" :name='item.id' v-for="item of classlist" :key="item.id">
-                <van-list
-                    v-model="loading"
-                    :finished="finished"
-                    finished-text="没有更多了"
-                    @load="onLoad"
-                    >
-                    <div class="goodsDiv" v-for="(itemTwo,index) of goodslist" :key='index'>
-                        <div>
-                            {{itemTwo.id}}
-                        </div>
-                    </div>
-                </van-list>
-            </van-tab>
-        </van-tabs>
-        
-    </div>
+  <div v-if="classlist.length>1">
+    <div></div>
+    <van-tabs v-model="active" @click="onClick">
+      <van-tab :title="item.cate_name" :name="item.id" v-for="item of classlist" :key="item.id">
+        <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
+          <div class="goodsDiv" v-for="(itemTwo,index) of goodslist" :key="index">
+            <div>{{itemTwo.id}}</div>
+          </div>
+        </van-list>
+      </van-tab>
+    </van-tabs>
+  </div>
 </template>
 
 <script>
 export default {
-    components:{},
-    props:{},
-    data(){
-      return {
-            classlist: [
-                {id: 0, cate_name:'全部'}
-            ],
-            classId: 0,
-            page: 0,
-            active: 0,
-            goodslist: [],
-            loading: false,
-            finished: false,
-      }
+  components: {},
+  props: {},
+  data() {
+    return {
+      classlist: [{ id: 0, cate_name: "全部" }],
+      classId: 0,
+      page: 0,
+      active: 0,
+      goodslist: [],
+      loading: false,
+      finished: false
+    };
+  },
+  watch: {},
+  computed: {},
+  methods: {
+    //获取分类
+    getclass() {
+      let parms = {
+        method: "get.goods.category.list"
+      };
+      this.$post("/api/v1/goodsCategory", parms)
+        .then(res => {
+          // console.log(res)
+          for (let item of res.data) {
+            this.classlist.push(item);
+          }
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
     },
-    watch:{},
-    computed:{},
-    methods:{
-        //获取分类
-        getclass(){
-            let parms = {
-                method: 'get.goods.category.list',
-            };
-            this.$post('/api/v1/goodsCategory', parms)
-                .then((res) => {
-                    // console.log(res)
-                    for(let item of res.data){
-                        this.classlist.push(item)
-                    }
-                }).catch(function (error) {
-                    console.log(error);
-                })
-        },
-        //分类点击
-        onClick(name, title){
-            console.log(name)
-            this.classId = name;
-            this.page= 0;
-            this.goodslist = [];
-            // 清空列表数据
-            this.finished = false;
-            // 重新加载数据
-            // 将 loading 设置为 true，表示处于加载状态
-            this.loading = true;
-            this.onLoad();
-        },
-        
-        //获取数据
-        onLoad(  ) {
-            let parms = {
-                method: 'get.goods.map.list',
-                map: 'vip',
-                page: this.page,
-                page_size: 10,
-                cate_id: this.classId==0?'':this.classId
-            };
-            // console.log(parms);
-            this.$post('/api/v1/goods', parms)
-                .then((res) => {
-                    if(res.data){
-                         this.page += res.data.length;
-                        // console.log(this.page);
-                        this.goodslist = [...this.goodslist, ...res.data];
-                        // 加载状态结束
-                        this.loading = false;
-                        // 数据全部加载完成
-                        if (res.data.length< 10) {
-                            this.finished = true;
-                        }
-                    }else{
-                        // console.log('我是null')
-                        this.finished = true;
-                    }
-                    // console.log(res)
-                   
-                }).catch(function (error) {
-                    console.log(error);
-            })
+    //分类点击
+    onClick(name, title) {
+      console.log(name);
+      this.classId = name;
+      this.page = 0;
+      this.goodslist = [];
+      // 清空列表数据
+      this.finished = false;
+      // 重新加载数据
+      // 将 loading 设置为 true，表示处于加载状态
+      this.loading = true;
+      this.onLoad();
+    },
 
-        },
+    //获取数据
+    onLoad() {
+      let parms = {
+        method: "get.goods.map.list",
+        map: "vip",
+        page: this.page,
+        page_size: 10,
+        cate_id: this.classId == 0 ? "" : this.classId
+      };
+      // console.log(parms);
+      this.$post("/api/v1/goods", parms)
+        .then(res => {
+          if (res.data) {
+            this.page += res.data.length;
+            // console.log(this.page);
+            this.goodslist = [...this.goodslist, ...res.data];
+            // 加载状态结束
+            this.loading = false;
+            // 数据全部加载完成
+            if (res.data.length < 10) {
+              this.finished = true;
+            }
+          } else {
+            // console.log('我是null')
+            this.finished = true;
+          }
+          // console.log(res)
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
     },
-    created(){
-       this.getclass();
-    },
-    mounted(){
+
+    //获取地址
+    getLoca() {
+      this.getLocation(res => {
+        console.log(res, 123456, "获取位置成功");
+        if (res) {
+          this.latitude = res.latitude;
+          this.longitude = res.longitude;
+        } else {
+          console.log("获取位置失败！");
+          this.tc("获取位置失败，请检查您的GPS");
+          setTimeout(() => {
+            this.$router.back(-1);
+          }, 1000);
+        }
+      });
     }
-}
+  },
+  created() {
+      this.getLoca();
+    // this.getclass();
+  },
+  mounted() {}
+};
 </script>
 <style lang="scss" scoped>
-    .goodsDiv{
-        display: inline-block;
-        height: 300px;
-        width: 50%;
-        padding: 0.2rem;
-        line-height: 300px;
-        color: #fff;
-        >div{
-            border: 2px solid red;;
-            background-color: #121452;
-        }
-    }
+.goodsDiv {
+  display: inline-block;
+  height: 300px;
+  width: 50%;
+  padding: 0.2rem;
+  line-height: 300px;
+  color: #fff;
+  > div {
+    border: 2px solid red;
+    background-color: #121452;
+  }
+}
 </style>
