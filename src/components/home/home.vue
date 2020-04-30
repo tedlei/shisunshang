36a2875831626eba0bd6578d9983255ed217605b
cjy <www.cjy163.com@qq.com>
2020-04-30 14:17:31 +0800

    4月底

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -2,185 +2,152 @@
   <div class="content main_box">
     <!--  头部  -->
     <header>
-      <el-row
+      <van-row
         type="flex"
         class="row-bg"
         justify="space-between"
         style="align-items: center;width: 100%"
       >
-        <!-- <el-dropdown trigger="click">
-          <span class="el-dropdown-link">
-            重庆
-            <i class="el-icon-arrow-down el-icon--right"></i>
-          </span>
-          <el-dropdown-menu slot="dropdown">
-            <el-dropdown-item icon="el-icon-plus">重庆</el-dropdown-item>
-          </el-dropdown-menu>
-                </el-dropdown>-->
-                <!--  搜索组件    -->
-                <div @click="opensearch" style="width: 100%">
-                    <search :dmsg="msg"></search>
-                </div>
-                <!--扫码-->
-                <!-- <div class="sao">
-          <i class="el-icon-full-screen"></i>
-          <div>扫码</div>
-                </div>-->
-                <!--消息-->
-                <!-- <div class="news">
-          <router-link to="/news">
-            <i class="el-icon-chat-dot-round"></i>
-            <div>消息</div>
-          </router-link>
-                </div>-->
-            </el-row>
-        </header>
-        <!--  轮播组件  -->
-        <div class="banner">
-            <div class="swiper-container">
-                <div class="swiper-wrapper">
-                    <div class="swiper-slide" v-for="(item,index) in bannermsg" :key="index">
-                        <router-link :to="{path:item.url}">
-                            <img :src="item.img" style="border-radius: 5px" />
-                        </router-link>
-                    </div>
-                </div>
-            </div>
+        <!--  搜索组件    -->
+        <div @click="opensearch" style="width: 100%">
+          <search :dmsg="msg"></search>
         </div>
-
-        <!--  分类按钮  -->
-        <div style="padding:0.1rem">
-            <el-row :gutter="0" class="f-nav">
-                <el-col :span="5" v-for="(item, index) in categorylist" :key="index">
-                    <div
-                        class="item"
-                        @click="doting(item.id,item.module)"
-                        style="margin-bottom: 0.1rem"
-                    >
-                        <img :src="item.icon" />
-                        <p>{{item.cate_name}}</p>
-                    </div>
-                </el-col>
-            </el-row>
-        </div>
-        <!--  公告  -->
-        <div class="notice">
-            <img src="../../assets/img/icon1.png" />
-            <el-carousel
-                height="0.2rem"
-                direction="vertical"
-                style="margin-left:0.05rem;"
-                :autoplay="true"
-                >
-                <el-carousel-item v-for="(item,items) in news" :key="items">
-                    <router-link :to="{path:'/news/newsdetail',query:{id:item.id}}">{{ item.title }}</router-link>
-                </el-carousel-item>
-            </el-carousel>
-            <!-- <span class="clo-g"><router-link to="/news">查看</router-link></span> -->
-        </div>
-
-        <!--  商品列表  -->
-        <div class="list" v-for="(item,index) in lists" :key="index">
-            <div class="top_name">
-                <span>{{item.module}}</span>
-                <span class="text_rich">{{item.message}}</span>
-            </div>
-            <router-link :to="{path:item.ad.url}">
-                <img :src="item.ad.img" style="padding-bottom: 0.1rem" />
+        <!--扫码-->
+      </van-row>
+    </header>
+    <!--  轮播组件  -->
+    <div class="banner">
+      <div class="swiper-container">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide" v-for="(item,index) in bannermsg" :key="index">
+            <router-link :to="{path:item.url}">
+              <img :src="item.img" style="border-radius: 5px"/>
             </router-link>
-
-            <van-row class="introduce_img">
-                <router-link
-                    v-for="(goods, index) in item.goods"
-                    :key="index"
-                    :to="{path:'/goodsdetails',query:{id:goods.id}}"
-                    >
-                    <van-col :span="12" class="lists">
-                        <div class="grid-content bg-purple">
-                            <div class="goodsk" ref="imgWidth" :style="{'height':boxheight}">
-                                <div v-if="goods.is_show == 1">
-                                    <img src="../../assets/img/goodsk.jpg" class="goodsk_img" />
-                                    <p class="goodsk_price">￥{{Number(goods.price)}}</p>
-                                    <p class="goodsk_p">签到金可订购</p>
-                                </div>
-                                <van-image width="100%" :src="goods.imgsrc" />
-                            </div>
-                            <div class="msg">
-                                <div class="text fontWrap fontWrapTwo">{{goods.name}}</div>
-                                <!--                <div class="clo-g price">{{goods.price}}</div>-->
-                            </div>
-                        </div>
-                    </van-col>
-                </router-link>
-            </van-row>
+          </div>
         </div>
-        <!--  -->
-        <!-- <signin></signin> -->
 
-        <!-- <div class="qrCodeMax" v-show="qrCodeShow" @click="Previewcode(true)">
-            <div class="topBack">
-                <van-icon name="arrow-left" size="28" />
-                <span style="font-size:16px;color:rgb(15,15,15)">石笋山生态平台</span>
-                <div></div>
-            </div>
-            <div id="qrHome" class="qrconde">
-                <img src="http://test.gj.wjeys.com/public/static/vue/weixin_code.jpg" />
-            </div>
-        </div> -->
+      </div>
+      <div class="swiper-pagination"></div>
     </div>
+
+    <!--  分类按钮  -->
+    <div style="padding:0.1rem">
+      <el-row :gutter="0" class="f-nav">
+        <el-col :span="5" v-for="(item, index) in categorylist" :key="index">
+          <div
+            class="item"
+            @click="doting(item.id,item.module)"
+            style="margin-bottom: 0.1rem"
+          >
+            <img :src="item.icon"/>
+            <p>{{item.cate_name}}</p>
+          </div>
+        </el-col>
+      </el-row>
+    </div>
+    <!--  公告  -->
+
+    <div class="notice">
+      <img src="../../assets/img/icon1.png"/>
+
+      <van-swipe style="height: 0.2rem;margin-left: 0.05rem" :autoplay="2000" :show-indicators="false" vertical>
+        <van-swipe-item v-for="(item,items) in news" :key="items">
+          <router-link :to="{path:'/news/newsdetail',query:{id:item.id}}">{{ item.title }}</router-link>
+        </van-swipe-item>
+      </van-swipe>
+
+    </div>
+
+    <!--  商品列表  -->
+    <div class="goods_lists">
+      <div class="list" v-for="(item,index) in lists" :key="index">
+        <div class="top_name">
+          <span>{{item.module}}</span>
+          <span class="text_rich">{{item.message}}</span>
+        </div>
+        <router-link :to="{path:item.ad.url}">
+          <img :src="item.ad.img" style="padding-bottom: 0.1rem"/>
+        </router-link>
+
+        <van-row class="introduce_img">
+          <router-link
+            v-for="(goods, index) in item.goods"
+            :key="index"
+            :to="{path:'/goodsdetails',query:{id:goods.id}}"
+          >
+            <van-col :span="12" class="lists">
+              <div class="grid-content bg-purple">
+                <div class="goodsk" ref="imgWidth" :style="{'height':boxheight}">
+                  <div v-if="goods.is_show == 1">
+                    <img src="../../assets/img/goodsk.jpg" class="goodsk_img"/>
+                    <p class="goodsk_price">￥{{Number(goods.price)}}</p>
+                    <p class="goodsk_p">签到金可订购</p>
+                  </div>
+                  <van-image width="100%" :src="goods.imgsrc"/>
+                </div>
+                <div class="msg">
+                  <div class="text fontWrap fontWrapTwo">{{goods.name}}</div>
+                  <!--                <div class="clo-g price">{{goods.price}}</div>-->
+                </div>
+              </div>
+            </van-col>
+          </router-link>
+        </van-row>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-//1.先使用import导入你要在该组件中使用的子组件
-import Carousel from "../carousel-cp/carousel";
-import Search from "../search/search";
-import Bus from "../../assets/js/bus";
-import Swiper from "swiper";
-import Signin from "../Signin/Signin.vue";
-import address from "../../../static/address";
+    //1.先使用import导入你要在该组件中使用的子组件
+    import Carousel from "../carousel-cp/carousel";
+    import Search from "../search/search";
+    import Bus from "../../assets/js/bus";
+    import Swiper from "swiper";
+    import Signin from "../Signin/Signin.vue";
 
-export default {
-    name: "home",
-    //2.然后,在components中写入子组件
-    components: { Signin, Search, Carousel },
-    data() {
-        return {
-            msg: "首页",
-            name: [
-                "顾客区",
-                "会员区",
-                "零售区",
-                "商家区",
-                "签到区",
-                "顾客区",
-                "会员区",
-                "零售区",
-                "商家区",
-                "签到区"
-            ],
-            categorylist: [],
-            bannermsg: [],
-            lists: [],
-            news: [],
-            boxheight: "",
-            // qrCodeShow: false
-        };
-    },
-    methods: {
-        //显示二维码
-        Previewcode: function(boo) {
-            this.$router.push({path:'/codePage'})
-            // if (boo) {
-            //     setTimeout(() => {
-            //         this.qrCodeShow = false;
-            //     }, 500);
-            // } else this.qrCodeShow = true;
+    export default {
+        name: "home",
+        //2.然后,在components中写入子组件
+        components: {Search, Carousel},
+        data() {
+            return {
+                msg: "首页",
+                name: [
+                    "顾客区",
+                    "会员区",
+                    "零售区",
+                    "商家区",
+                    "签到区",
+                    "顾客区",
+                    "会员区",
+                    "零售区",
+                    "商家区",
+                    "签到区"
+                ],
+                categorylist: [],
+                bannermsg: [],
+                lists: [],
+                news: [],
+                boxheight: "",
+                // qrCodeShow: false
+            };
         },
-        imgLoad(index) {
-            let doc = document.getElementById("imgId" + index);
-            let width = this.$refs.imgWidth[index].clientWidth;
-            doc.style.height = width + "px";
-        },
+        methods: {
+            //显示二维码
+            Previewcode: function (boo) {
+                this.$router.push({path: '/codePage'})
+                // if (boo) {
+                //     setTimeout(() => {
+                //         this.qrCodeShow = false;
+                //     }, 500);
+                // } else this.qrCodeShow = true;
+            },
+            imgLoad(index) {
+                let doc = document.getElementById("imgId" + index);
+                let width = this.$refs.imgWidth[index].clientWidth;
+                doc.style.height = width + "px";
+            },
 
             //获取首页
             getHomeMsg: function () {
@@ -192,6 +159,40 @@ export default {
                 this.$post("/api/v1/ad", ad_data)
                     .then(response => {
                         this.bannermsg = response.data;
+                        this.$nextTick(() => {
+                            let mySwiper = new Swiper(".swiper-container", {
+                                pagination: {
+                                    el: '.swiper-pagination',
+                                    clickable: true,
+                                },
+                                slidesPerView: 1.15,
+                                observer: true,
+                                slidesOffsetAfter: 13,
+                                direction: "horizontal",
+                                observeParents: true,
+                                spaceBetween: 20,
+                                loop: true,
+                                centeredSlides: true,
+                                effect: "coverflow",
+                                grabCursor: true,
+                                watchSlidesProgress: true,
+                                loopedSlides: 5,
+                                autoplay: {
+                                    delay: 3000, //自动播放速度
+                                    disableOnInteraction: false //鼠标移上去时是否还继续播放
+                                },
+                                coverflowEffect: {
+                                    rotate: 0,
+                                    stretch: 0,
+                                    depth: 30,
+                                    modifier: 3,
+                                    slideShadows: false
+                                },
+                                speed: 1000,
+                                initialSlide: 0,
+                                // lazy: {loadPrevNext: true,},
+                            });
+                        })
                     })
                     .catch(function (error) {
                         console.log(error);
@@ -234,7 +235,7 @@ export default {
                         if (response.data.is_sign == 0) {
                             this.$dialog.confirm({
                                 title: '嗨！',
-                                message: '今日未签到，是否签到赢好礼呢！'
+                                message: '今日还未签到是否签到赢好礼呢！'
                             }).then(() => {
                                 this.$router.push({
                                     path: '/Signin'
@@ -268,66 +269,37 @@ export default {
                 }
 
             },
-        //搜索
-        opensearch: function() {
-            Bus.$emit("searchval", true);
-        }
-    },
-    created() {
-        let users = JSON.parse(localStorage.getItem("userinfo"));
-        if (!users.hrl_openid) {
-            if(location.host==address[0].domain){
+            //搜索
+            opensearch: function () {
+                Bus.$emit("searchval", true);
+            }
+        },
+        created() {
+            let users = JSON.parse(localStorage.getItem("userinfo"));
+            if (!users.hrl_openid) {
                 this.$router.push("/authorTwo");
             }
+        },
+        updated() {
+
+        },
+        mounted() {
+
+            let token = localStorage.getItem("usertoken");
+            if (token) {
+                this.getHomeMsg();
+            }
+            ;
+            this.getsign()
         }
-        setTimeout(() => {
-            const mySwiper = new Swiper(".swiper-container", {
-                slidesPerView: 1.15,
-                observer: true,
-                slidesOffsetAfter: 15,
-                direction: "horizontal",
-                observeParents: true,
-                spaceBetween: 20,
-                loop: true,
-                centeredSlides: true,
-                effect: "coverflow",
-                grabCursor: true,
-                watchSlidesProgress: true,
-                loopedSlides: 5,
-                autoplay: {
-                    delay: 3000, //自动播放速度
-                    disableOnInteraction: false //鼠标移上去时是否还继续播放
-                },
-                coverflowEffect: {
-                    rotate: 0,
-                    stretch: 0,
-                    depth: 30,
-                    modifier: 3,
-                    slideShadows: false
-                },
-                speed: 1000,
-                initialSlide: 1,
-                // lazy: {loadPrevNext: true,},
-                pagination: {
-                    el: ".swiper-pagination"
-                }
-            });
-        }, 300);
-    },
-    mounted() {
-        let token = localStorage.getItem("usertoken");
-        if (token) {
-            this.getHomeMsg();
-        };
-        this.getsign()
-    }
-};
+    };
 </script>
 
 <style lang="scss" scoped>
   .content {
     top: 0;
     bottom: 0.65rem;
+
     header {
       text-align: left;
       padding: 0 0.1rem;
@@ -353,6 +325,26 @@ export default {
       }
     }
 
+    /* 广告 */
+    .banner {
+      > > > .swiper-pagination {
+        position: inherit;
+
+        .swiper-pagination-bullet {
+          width: 0.25rem;
+          height: 3px;
+          border-radius: 0;
+          margin: 0 5px;
+          outline: 0;
+        }
+
+        .swiper-pagination-bullet-active {
+          background: #009900;
+
+        }
+      }
+    }
+
     /* 分类按钮 */
 
     .f-nav div img {
@@ -368,10 +360,6 @@ export default {
       // justify-content: space-between;
       align-items: center;
       text-align: left;
-
-      .el-carousel {
-        width: 80%;
-      }
 
       h3 {
         font-weight: normal;
@@ -400,10 +388,6 @@ export default {
       }
     }
 
-    .el-carousel__indicators--vertical {
-      display: none !important;
-    }
 
-    
-}
+  }
 </style>
--- src/components/mine/myShare/PosterStand.vue
+++ src/components/mine/myShare/PosterStand.vue
@@ -1,100 +1,100 @@
 <template>
-    <div class="content">
-        <div class='imgdivfff'>
-            <div class="imgDIv" v-for="(item,index) of imgArr" :key="index" @click="ImagePreview(index)">
-                <img style="height:3.2rem;" :src="item.url" alt="">
-            </div>
-        </div>
-        <van-overlay style="" :show="show" @click="Closehb">
-            <div class="wrapper">
-                <div></div>
-                <div id="Posters" v-show="!showTwo">
-                    <!-- <div id="qrCode" ref="qrCode"></div> -->
-                    <div :style="'top:'+top+';'+'left:'+left">
-                        <qrcode :url="urls" 
-                            :wid="wid" 
-                            :hei="hei"
-                            :imgwid="50"
-                            :imghei="50">
-                        </qrcode>
-                    </div>
-                    <!-- <img @load="olad" ref="zjhb" :src="bkUrl" alt=""> -->
-                    <img @load="olad" ref="zjhb" :src="bkUrl" alt="">
-                </div>
-                <div v-show="showTwo">
-                    <img :style="'height:'+ h + 'px'" :src="hbUrl" alt="">
-                </div>
-            </div>
-        </van-overlay>
-
+  <div class="content">
+    <div class='imgdivfff'>
+      <div class="imgDIv" v-for="(item,index) of imgArr" :key="index" @click="ImagePreview(index)">
+        <img style="height:3.2rem;" :src="item.url" alt="">
+      </div>
     </div>
+    <van-overlay style="" :show="show" @click="Closehb">
+      <div class="wrapper">
+        <div></div>
+        <div id="Posters" v-show="!showTwo">
+          <!-- <div id="qrCode" ref="qrCode"></div> -->
+          <div :style="'top:'+top+';'+'left:'+left">
+            <qrcode :url="urls"
+                    :wid="wid"
+                    :hei="hei"
+                    :imgwid="50"
+                    :imghei="50">
+            </qrcode>
+          </div>
+          <!-- <img @load="olad" ref="zjhb" :src="bkUrl" alt=""> -->
+          <img @load="olad" ref="zjhb" :src="bkUrl" alt="">
+        </div>
+        <div v-show="showTwo">
+          <img :style="'height:'+ h + 'px'" :src="hbUrl" alt="">
+        </div>
+      </div>
+    </van-overlay>
+
+  </div>
 </template>
 
 <script>
     import html2canvas from "html2canvas";
     import qrcode from 'vue_qrcodes'
     export default {
-    components: {
-        qrcode
-    },
-    data () {
-        return {
-            imgArr: [
-                {
-                    url:require('../../../assets/img/zjhb1.jpg'),
-                    top: '50%',
-                    left: '50%',
-                },
-                {
-                    url:require('../../../assets/img/zjhb2.jpg'),
-                    top: '73.33%',
-                    left: '50.06%',
-                }
-            ],
-            index: null,
-            urls: null,
-            portrait: '',
-            show:false,
-            showTwo: false,
-            hbUrl:'',
-            h: '',
-            bkUrl: '',
-            top:'50%',
-            left: '50%',
-            wid: 170,
-            hei: 170
-        }
-    },
-    methods: {
-        creatQrCode() {
-            let arr = location.href.split('/');
-            let QRcodeDomainName = arr[0]+"//"+arr[2];
-            let userinfo = JSON.parse(this.$store.getters.getuserinfo);
-            let qrCode = new QRCode('qrCode', {
-                text: QRcodeDomainName + '/?state=' + userinfo.referee_number, // 需要转换为二维码的内容
-                width: 170,
-                height: 170,
-                colorDark: '#000000',
-                colorLight: '#ffffff',
-                correctLevel: QRCode.CorrectLevel.H
-            });
-            
+        components: {
+            qrcode
         },
-        ImagePreview( i ){
-            // console.log(i)
-            // console.log(this.index)
-            if(this.index == i){
-                this.showTwo = true;
+        data () {
+            return {
+                imgArr: [
+                    {
+                        url:require('../../../assets/img/zjhb1.jpg'),
+                        top: '50%',
+                        left: '50%',
+                    },
+                    {
+                        url:require('../../../assets/img/zjhb2.jpg'),
+                        top: '73.33%',
+                        left: '50.06%',
+                    }
+                ],
+                index: null,
+                urls: null,
+                portrait: '',
+                show:false,
+                showTwo: false,
+                hbUrl:'',
+                h: '',
+                bkUrl: '',
+                top:'50%',
+                left: '50%',
+                wid: 170,
+                hei: 170
             }
-            this.index = i;
-            this.show = true;
-            this.bkUrl = this.imgArr[i].url;
-            this.top = this.imgArr[i].top;
-            this.left = this.imgArr[i].left;
-            // this.olad();
         },
-        olad(e){
-            // console.log(this.$refs.zjhb.width*20);
+        methods: {
+            creatQrCode() {
+                let arr = location.href.split('/');
+                let QRcodeDomainName = arr[0]+"//"+arr[2];
+                let userinfo = JSON.parse(this.$store.getters.getuserinfo);
+                let qrCode = new QRCode('qrCode', {
+                    text: QRcodeDomainName + '/?state=' + userinfo.referee_number, // 需要转换为二维码的内容
+                    width: 170,
+                    height: 170,
+                    colorDark: '#000000',
+                    colorLight: '#ffffff',
+                    correctLevel: QRCode.CorrectLevel.H
+                });
+
+            },
+            ImagePreview( i ){
+                // console.log(i)
+                // console.log(this.index)
+                if(this.index == i){
+                    this.showTwo = true;
+                }
+                this.index = i;
+                this.show = true;
+                this.bkUrl = this.imgArr[i].url;
+                this.top = this.imgArr[i].top;
+                this.left = this.imgArr[i].left;
+                // this.olad();
+            },
+            olad(e){
+                // console.log(this.$refs.zjhb.width*20);
                 let Poster = document.getElementById('Posters');
                 // console.dir(Poster.offsetWidth);
                 // console.dir(Poster.offsetHeight);
@@ -116,72 +116,72 @@
                         this.showTwo = true;
                     }
                 );
-            
+
+            },
+            Closehb(){
+                this.show = false;
+                this.showTwo = false;
+                // console.log(this.showTwo)
+            },
         },
-        Closehb(){
-            this.show = false;
-            this.showTwo = false;
-            // console.log(this.showTwo)
+        created(){
+            // this.$nextTick(() => {
+            //     this.creatQrCode();
+            // });
+            let arr = location.href.split('/');
+            let QRcodeDomainName = arr[0]+"//"+arr[2];
+            let userinfo = JSON.parse(this.$store.getters.getuserinfo);
+            this.portrait = userinfo.portrait;
+            this.urls = QRcodeDomainName + '/?state=' + userinfo.referee_number;
+
         },
-    },
-    created(){
-        // this.$nextTick(() => {
-        //     this.creatQrCode();
-        // });
-        let arr = location.href.split('/');
-        let QRcodeDomainName = arr[0]+"//"+arr[2];
-        let userinfo = JSON.parse(this.$store.getters.getuserinfo);
-        this.portrait = userinfo.portrait;
-        this.urls = QRcodeDomainName + '/?state=' + userinfo.referee_number;
-        
-    },
-    computed: {
-        
-    },
-    watch: {
-        
+        computed: {
+
+        },
+        watch: {
+
+        }
     }
-}
 </script>
 <style lang="scss" scoped>
-    .qrcode{
-        border: 4px solid #fff !important;
-        > > > .logoimg{
-            border: 3px solid #fff !important;
-        }
+  .qrcode{
+    border: 4px solid #fff !important;
+    > > > .logoimg{
+      border: 3px solid #fff !important;
     }
-    .imgdivfff{
-        font-size: 0;
-        padding: 0.1rem 0.05rem;
+  }
+  .imgdivfff{
+    font-size: 0;
+    padding: 0.1rem 0.05rem;
+  }
+  .imgDIv{
+    display: inline-block;
+    width: 50%;
+    padding: 0.05rem;
+    background-size: 100%;
+    // background-color: aqua;
+
+  }
+  .wrapper {
+    height: 100%;
+    overflow-y: scroll;
+    -webkit-overflow-scrolling:touch;
+    padding: 0 0.1rem;
+    >div:first-child{
+      height: 0.48rem;
     }
-    .imgDIv{
-        display: inline-block;
-        width: 50%;
-        padding: 0.05rem;
-        background-size: 100%;
-        // background-color: aqua;
-        
+    >div:nth-child(2){
+      position: relative;
+      >div:first-child{
+        position: absolute;
+        // left: 50%; top: 50%;
+        margin-top: -85px;
+        margin-left: -85px;
+      }
+      >img{
+        width: 100%;
+      }
     }
-    .wrapper {
-        height: 100%;
-        overflow-y: scroll;
-        -webkit-overflow-scrolling:touch;
-        padding: 0 0.1rem;
-        >div:first-child{
-            height: 0.48rem;
-        }
-        >div:nth-child(2){
-            position: relative;
-            >div:first-child{
-                position: absolute;
-                // left: 50%; top: 50%;
-                margin-top: -85px;
-                margin-left: -85px;
-            }
-            >img{
-                width: 100%;
-            }
-        }
-       
-    }
+
+  }
 </style>
